efccad99e833d402a08bcd284f576321f00b8bc3
Stanislav Sorochenko <81685498+Staks-sor@users.noreply.github.com>
2023-07-20 12:31:11 +0300

    Create 1. Two Sum.py

---
+++ 1. Two Sum/1. Two Sum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
