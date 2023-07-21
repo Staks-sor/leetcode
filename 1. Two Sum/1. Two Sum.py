507afbf61cfbfb45f5f0b97f7f1fe43aa16229e7
Stanislav Sorochenko <81685498+Staks-sor@users.noreply.github.com>
2023-07-21 08:26:37 +0300

    Update 1. Two Sum.py
    прокомментировал код

--- 1. Two Sum/1. Two Sum.py
+++ 1. Two Sum/1. Two Sum.py
@@ -2,9 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_dict:
-                return [num_dict[complement], i]
-            num_dict[num] = i
+        num_dict = {}  # Создаем пустой словарь для хранения чисел и их индексов
+        for i, num in enumerate(nums):  # Перебираем список с помощью функции enumerate()
+            complement = target - num  # Вычисляем значение complement
+            if complement in num_dict:  # Проверяем, находится ли complement уже в словаре
+                return [num_dict[complement], i]  # Если нашли пару, возвращаем индексы чисел
+            num_dict[num] = i  # Добавляем число и его индекс в словарь
+        return []  # Если не найдено пар чисел, возвращаем пустой список
+
+
